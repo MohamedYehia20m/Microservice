4281b04075e59295b162da70cd03ec3db1940643
MohamedYehia20m <medoyehia2001@gmail.com>
2025-02-24 21:29:51 +0200

    add Factory bean for model mapper

--- accounts/accounts/src/main/java/com/eazybytes/accounts/config/FactoryBean.java
+++ accounts/accounts/src/main/java/com/eazybytes/accounts/config/FactoryBean.java
@@ -1,4 +1,14 @@
 package com.eazybytes.accounts.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FactoryBean {
+
+    @Bean
+    public ModelMapper factoryBean() {
+        return new ModelMapper();
+    }
 }
