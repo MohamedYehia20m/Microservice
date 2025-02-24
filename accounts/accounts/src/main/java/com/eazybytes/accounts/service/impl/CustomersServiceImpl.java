a10fdceb92c3f0f60ec9ae4ee40d190fbe15175a
MohamedYehia20m <medoyehia2001@gmail.com>
2025-02-24 21:45:33 +0200

    refactor service implementation

--- accounts/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
+++ accounts/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
@@ -1,4 +1,55 @@
 package com.eazybytes.accounts.service.impl;
 
-public class CustomersServiceImpl {
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.ICustomersService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private CustomerRepository customerRepository;
+    final ModelMapper modelMapper;
+
+    public void addCustomer(CustomerDto customerDto) {
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        customerRepository.save(modelMapper.map(customerDto, Customer.class));
+    }
+
+    public CustomerDto fetchCustomer(String mobileNumber) {
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(mobileNumber);
+        return optionalCustomer.map(customer -> modelMapper.map(customer, CustomerDto.class)).orElse(null);
+    }
+
+    public boolean updateCustomer(CustomerDto customerDto) {
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            Customer customer = CustomerMapper.mapToCustomer(customerDto, optionalCustomer.get());
+            customerRepository.save(customer);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteCustomer(String mobileNumber) {
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(mobileNumber);
+        if(optionalCustomer.isPresent()) {
+            customerRepository.delete(optionalCustomer.get());
+            return true;
+        }
+        return false;
+    }
+
 }
